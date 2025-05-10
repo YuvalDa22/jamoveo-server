4cf966f9fef54ed146450bf44101166b9953a93b
Yuval Dadon <yuvidadon@gmail.com>
2025-05-10 09:06:19 +0300

    add users list - sockets

--- src/sockets/socket.controller.js
+++ src/sockets/socket.controller.js
@@ -1,11 +1,22 @@
+const sessionUsers = {};
+
 export default function socketHandler(io) {
     io.on('connection', (socket) => {
         console.log('New client connected:', socket.id);
     
         // Join a session room
-        socket.on('join-session', ({ sessionId, username }) => {
+        socket.on('join-session', ({ sessionId, username, role }) => {
           socket.join(sessionId);
           console.log(`${username} joined session ${sessionId}`);
+
+          // save the user info - to show who is in the session
+          if (!sessionUsers[sessionId]) sessionUsers[sessionId] = [];
+          if (!sessionUsers[sessionId].some(user => user.username === username)) {
+            sessionUsers[sessionId].push({ username, role });
+          }
+
+          // Emit updated user list to all in room
+          io.to(sessionId).emit("session-users-updated", sessionUsers[sessionId]);
         });
     
         // Load a song (admin)
