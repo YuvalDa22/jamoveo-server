92485da01ff7d76d85d5294c05989f52839eadae
Yuval Dadon <yuvidadon@gmail.com>
2025-05-10 16:34:50 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -29,6 +29,12 @@ app.use(cors());
 app.use(express.json());
 app.use(setupAsyncLocalStorage);
 
+// debug middleware
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/session', sessionRoutes);
 app.use('/api/song', songRoutes);
