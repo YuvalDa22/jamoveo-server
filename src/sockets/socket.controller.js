7b3bb1b262c785385228fe79a8ab28e8268d85e3
Yuval Dadon <yuvidadon@gmail.com>
2025-05-10 11:59:56 +0300

    add countdown event on sockets

--- src/sockets/socket.controller.js
+++ src/sockets/socket.controller.js
@@ -1,33 +1,39 @@
 const sessionUsers = {};
 
 export default function socketHandler(io) {
-    io.on('connection', (socket) => {
-        console.log('New client connected:', socket.id);
-    
-        // Join a session room
-        socket.on('join-session', ({ sessionId, username, role }) => {
-          socket.join(sessionId);
-          console.log(`${username} joined session ${sessionId}`);
+  io.on("connection", (socket) => {
+    console.log("New client connected:", socket.id);
 
-          // save the user info - to show who is in the session
-          if (!sessionUsers[sessionId]) sessionUsers[sessionId] = [];
-          if (!sessionUsers[sessionId].some(user => user.username === username)) {
-            sessionUsers[sessionId].push({ username, role });
-          }
+    // Join a session room
+    socket.on("join-session", ({ sessionId, username, role }) => {
+      socket.join(sessionId);
+      console.log(`${username} joined session ${sessionId}`);
 
-          // Emit updated user list to all in room
-          io.to(sessionId).emit("session-users-updated", sessionUsers[sessionId]);
-        });
-    
-        // Load a song (admin)
-        socket.on('load-song', ({ sessionId, song }) => {
-          console.log(`Song loaded for session ${sessionId}:`, song);
-          socket.to(sessionId).emit('song-updated', song);
-        });
-    
-        // Disconnect
-        socket.on('disconnect', () => {
-          console.log('Client disconnected:', socket.id);
-        });
-      });
+      // save the user info - to show who is in the session
+      if (!sessionUsers[sessionId]) sessionUsers[sessionId] = [];
+      if (!sessionUsers[sessionId].some((user) => user.username === username)) {
+        sessionUsers[sessionId].push({ username, role });
+      }
+
+      // Emit updated user list to all in room
+      io.to(sessionId).emit("session-users-updated", sessionUsers[sessionId]);
+    });
+
+    // Load a song (admin)
+    socket.on("load-song", ({ sessionId, song }) => {
+      console.log(`Song loaded for session ${sessionId}:`, song);
+      socket.to(sessionId).emit("song-updated", song);
+    });
+
+    // Start countdown (triggered by admin)
+    socket.on("start-countdown", ({ sessionId }) => {
+      console.log(`Countdown started for session ${sessionId}`);
+      io.to(sessionId).emit("countdown-started");
+    });
+
+    // Disconnect
+    socket.on("disconnect", () => {
+      console.log("Client disconnected:", socket.id);
+    });
+  });
 }
